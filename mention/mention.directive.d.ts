dbf4dc9bfcdddaaa7a8cf9d81c2439c8982f7d85
Guerry Lim <glim@bizflow.com>
2017-12-08 11:27:18 -0500

    added dist folder

---
+++ mention/mention.directive.d.ts
@@ -0,0 +1,43 @@
+import { ElementRef, ComponentFactoryResolver, ViewContainerRef, TemplateRef } from "@angular/core";
+import { EventEmitter, OnInit, OnChanges, SimpleChanges } from "@angular/core";
+import { MentionListComponent } from './mention-list.component';
+/**
+ * Angular 2 Mentions.
+ * https://github.com/dmacfarlane/angular-mentions
+ *
+ * Copyright (c) 2017 Dan MacFarlane
+ */
+export declare class MentionDirective implements OnInit, OnChanges {
+    private _element;
+    private _componentResolver;
+    private _viewContainerRef;
+    mention: any[] | any;
+    mentionConfig: any;
+    mentionListTemplate: TemplateRef<any>;
+    searchTerm: EventEmitter<{}>;
+    private triggerChar;
+    private labelKey;
+    private disableSearch;
+    private maxItems;
+    private mentionSelect;
+    searchString: string;
+    startPos: number;
+    items: any[];
+    itemsObject: any;
+    startNode: any;
+    searchList: MentionListComponent;
+    stopSearch: boolean;
+    iframe: any;
+    keyCodeSpecified: boolean;
+    charPressed: string;
+    constructor(_element: ElementRef, _componentResolver: ComponentFactoryResolver, _viewContainerRef: ViewContainerRef);
+    ngOnInit(): void;
+    ngOnChanges(changes: SimpleChanges): void;
+    arrayContains(arrhaystack: any, needle: any): boolean;
+    setIframe(iframe: HTMLIFrameElement): void;
+    stopEvent(event: any): void;
+    blurHandler(event: any): void;
+    keyHandler(event: any, nativeElement?: HTMLInputElement): boolean;
+    updateSearchList(): void;
+    showSearchList(nativeElement: HTMLInputElement): void;
+}
